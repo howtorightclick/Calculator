97c544fc1b7e1032e9c2bec56a56ca95f31bc684
Joshua Chen <joshua.yingci.chen@gmail.com>
2025-08-15 23:59:57 +1000

    Added bracket and function tokenization

--- src/Tokenizer.cpp
+++ src/Tokenizer.cpp
@@ -2,6 +2,9 @@
 #include <cmath>
 #include <iostream>
 #include <memory>
+#include <expected>
+#include <set>
+#include <print>
 
 #include "Tokenizer.hpp"
 
@@ -9,55 +12,134 @@ Tokenizer::Tokenizer() {
 
 }
 
-void Tokenizer::tokenize(std::string &input) {
-    TokenType prev = NUMBER;
-
-    for (int i = 0; i < input.length(); i++) {
+std::expected<int, std::string> Tokenizer::tokenize(std::string &input) {
+    std::set<char> operators = {};
+    int i = 0;
+    while (i < input.length()) {
+        std::cout << i << std::endl;
         if (isdigit(input[i])) {
-            i = tokenizeNumber(input, i);
-            //TODO: error checking 
-
-            prev = NUMBER;
-        } else if (isalpha(input[i])) {
-            if (prev != OPERATOR) {
-                //TODO: throw invalid syntax error
+            auto result = tokenizeNumber(input, i);
+            if (result) {
+                i = result.value();
+            } else {
+                return result;
             }
-
-            
+        } else if (isOperator(input[i])) {
+            i = tokenizeOperator(input, i);
+        } else if (isalpha(input[i])) {
+            auto result = tokenizeFunction(input, i);
+            if (result) {
+                i = result.value();
+            } else {
+                return result;
+            }
+        } else if (input[i] == '(') {
+            i = tokenizeLeftBracket(input, i);
+        } else if (input[i] == ')') {
+            auto result = tokenizeRightBracket(input, i);
+            if (result) {
+                i = result.value();
+            } else {
+                return result;
+            }
+        } else {
+            std::unexpected("Syntax error: Unexpected symbol");
         }
     }
+
+    return 0;
 }
 
-int Tokenizer::tokenizeNumber(std::string &input, int &curr) {
-    bool isLastDigit = false, isDecimal = false;
-    int numDecimalPoints = 0;
+std::expected<int, std::string> Tokenizer::tokenizeNumber(const std::string &input, int &curr) noexcept {
+    int numDecimalMarkers = 0;
     int index = curr;
 
-    float res = 0;
-
-    while (!isLastDigit && index < input.length()) {
-        if (numDecimalPoints > 1) {
-            return -1;
-        }
+    //float res = 0;
+    std::string buffer = "";
+    while (index < input.length()) {
+        
         if (isdigit(input[index])) {
-            int digitShift = 1;
-            if (isDecimal) {
-                digitShift = pow(10, (curr - index) * -1);
-            } else {
-                digitShift = pow(10, curr - index);
-            }
-            res += input[index] * digitShift;
-
+            buffer.push_back(input[index]);
         } else if (input[index] == '.') {
-            isDecimal = true;
+            numDecimalMarkers += 1;
+            if (numDecimalMarkers > 1) {
+                return std::unexpected("Syntax error: More than one decimal markers");
+            } else {
+                buffer.push_back(input[index]);
+            }
+            //isDecimal = true;
         } else {
-            isLastDigit = true;
+            break;
         }
         index++;
     }
 
-    tokens.push_back(std::make_shared<NumberToken>(res));
+    tokens.push_back(std::make_shared<NumberToken>(std::stof(buffer)));
     return index; 
+}
+
+int Tokenizer::tokenizeOperator(const std::string &input, const int curr) {
+    tokens.push_back(std::make_shared<OperatorToken>(input[curr]));
+
+    return curr + 1;
+}
+
+std::expected<int, std::string> Tokenizer::tokenizeFunction(const std::string &input, int &curr) {
+    std::string buffer = "";
+    int i = curr;
+    while (i < input.length()) {
+        if (isalpha(input[i])) {
+            buffer.push_back(input[i]);
+        } else {
+            break;
+        }
+        i++;
+    }
+
+    char next = input[i];
+
+    if (next != '(') {
+        return std::unexpected("Syntax error: Missing bracket after function");
+    }
+
+    Function func;
+    if (buffer == "sin") {
+        func = SIN;
+    } else if (buffer == "cos") {
+        func = COS;
+    } else if (buffer == "tan") {
+        func = TAN;
+    } else if (buffer == "sinh") {
+        func = SINH;
+    } else if (buffer == "cosh") {
+        func = COSH;
+    } else if (buffer == "tanh") {
+        func = TANH;
+    } else {
+        return std::unexpected("Syntax error: Unknown function");
+    }
+
+    tokens.push_back(std::make_shared<FunctionToken>(func));
+
+    return i;
+}
+
+int Tokenizer::tokenizeLeftBracket(const std::string &input, const int curr) {
+    tokens.push_back(std::make_shared<LBracketToken>());
+    Tokenizer::leftBrackets.push(tokens.size() - 1);
+    return curr + 1;
+}
+
+std::expected<int, std::string> Tokenizer::tokenizeRightBracket(const std::string &input, const int curr) {
+    if (Tokenizer::leftBrackets.empty()) {
+        return std::unexpected("Syntax error: Mismatched brackets");
+    }
+    
+    int left = Tokenizer::leftBrackets.front();
+    Tokenizer::bracketMap[left] = curr;
+    Tokenizer::leftBrackets.pop();
+    tokens.push_back(std::make_shared<RBracketToken>());
+    return curr + 1;
 }
 
 void Tokenizer::printTokens() {
@@ -67,4 +149,8 @@ void Tokenizer::printTokens() {
     }
 
     std::cout << '\n';
+}
+
+bool Tokenizer::isOperator(char c) {
+    return std::find(OPERATORS, OPERATORS + 5, c) != OPERATORS + 5;
 }
--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -4,27 +4,57 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <expected>
+#include <queue>
+#include <unordered_map>
 
 #include "Token.hpp"
 #include "FunctionToken.hpp"
 #include "NumberToken.hpp"
 #include "OperatorToken.hpp"
+#include "LBracketToken.hpp"
+#include "RBracketToken.hpp"
 
 class Tokenizer {
 public:
     Tokenizer();
 
-    // Tokenize input string and return a vector of tokens
-    void tokenize(std::string &input);
+    // Tokenize input string
+    std::expected<int, std::string> tokenize(std::string &input);
 
+    // Print the list of tokens
     void printTokens();
-private:
-    // Finds the matching right bracket and returns its index
-    int tokenizeNumber(std::string &input, int &curr);
 
+    // Checks if the given character is a mathematical operator
+    static bool isOperator(char c);
+private:
+    // Creates a number token from the input string starting at curr and returns the new index.
+    // Guarantees that the resulting number is valid.
+    // Returns the new index.
+    std::expected<int, std::string> tokenizeNumber(const std::string &input, int &curr) noexcept;
+
+    // Creates an operator token from the input string starting at curr.
+    // Gurantees that expression is well formed.
+    int tokenizeOperator(const std::string &input, const int curr);
     //int findMatchingRightBracket(std::string &input, std::vector<int> &bracketMatch, int curr);
 
+    // Creates a function token from the input string starting at curr and returns the new index.
+    // Guarantees that expression is well formed.
+    // Returns the new index.
+    std::expected<int, std::string> tokenizeFunction(const std::string &input, int &curr);
+
+    // Creates a left bracket token from input string at curr and pushes its index onto a queue.
+    // Guarantees that expression is well formed.
+    // Returns the new index.
+    int tokenizeLeftBracket(const std::string &input, const int curr);
+
+    std::expected<int, std::string> tokenizeRightBracket(const std::string &input, const int curr);
+
     std::vector<std::shared_ptr<Token>> tokens;
+
+    std::queue<int> leftBrackets;
+
+    std::unordered_map<int, int> bracketMap;
 };
 
 #endif
