910bce2dae2aadefeabae2e6e2ed386edfef534b
Joshua Chen <joshua.yingci.chen@gmail.com>
2025-08-19 22:01:32 +1000

    Moved globals to globals.hpp

--- src/Token.hpp
+++ src/Token.hpp
@@ -2,10 +2,28 @@
 #define TOKEN_HPP
 
 #include <set>
+#include <memory>
+#include <expected>
+
+#include "Node.hpp"
+#include "Parser.hpp"
 
 //#include "Node.hpp"
+namespace STANDARD_SYMBOLS {
+    inline constexpr char OPERATORS[5] = {'+', '-', '/', '*', '^'};
 
-inline constexpr char OPERATORS[5] = {'+', '-', '/', '*', '^'};
+
+    inline constexpr const char* FUNCTIONS[7] = {
+        "sin",
+        "cos",
+        "tan",
+        "cotan",
+        "sinh",
+        "cosh",
+        "tanh"
+    };
+}
+
 
 enum Function {
     SIN,
@@ -29,10 +47,13 @@ enum TokenType {
 class Token {
 public:
     //virtual Node *toNode() = 0;
+    virtual ~Token() = default;
 
     virtual TokenType getType() = 0;
 
     virtual void printToken() = 0;
+
+    virtual std::expected<std::unique_ptr<Node>, std::string> parseToken(Parser &parser) = 0;
 };
 
 #endif
